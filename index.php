d5c8f4d4425aa12320ef76bef09328b0b5946473
Huy Le <jackle50098@gmail.com>
2024-03-03 20:47:45 -0500

    removed all routing from the index page and migrate to the new AppController Class

--- index.php
+++ index.php
@@ -17,64 +17,36 @@
 
 // Instantiate Fat-Free framework (F3)
     $f3 = Base::instance();
-    $form_validate = new FormValidation();
+    $form_validate = new FormControl();
     $data = new Data();
+    $controller  = new AppController($data,$form_validate);
 
-    // Default route
-    $f3->route('GET /',
-        function () {
-        $view = new Template();
-        echo $view->render('views/home.html');
+    // Route to home
+    $f3->route('GET /', function () {
+        $GLOBALS['controller']->home();
     });
 
     //Personal
     $f3->route('GET|POST /personal',
         function ($f3) use ($data, $form_validate) {
-        $requiredFields = $data->getPersonalInfoFields();
-        $nonRequiredFields = $data->getOptionalPersonalInfoFields();
-        $form_validate->processFormData($f3, $requiredFields, $nonRequiredFields, '/experience');
-        $view = new Template();
-        echo $view->render('views/personal.html');
+            $GLOBALS['controller']->personal($f3);
     });
 
     //experience
     $f3->route('GET|POST /experience', function ($f3) use ($data, $form_validate) {
-
-        $requiredFields = $data->getExperienceFields();
-        $nonRequiredFields = $data->getOptionalExperienceFields();
-        $form_validate->processFormData($f3, $requiredFields, $nonRequiredFields, '/mail');
-
-        $view = new Template();
-        echo $view->render('views/experience.html');
+        $GLOBALS['controller']->experience($f3);
     });
 
     //Mail
     $f3->route('GET|POST /mail',
         function ($f3) {
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            $selected_technologies = $_POST['tech'] ?? [];
-            $selected_industries = $_POST['ind'] ?? [];
-            $_SESSION['selected_technologies'] = $selected_technologies;
-            $_SESSION['selected_industries'] = $selected_industries;
-            $f3->set('selected_technologies', $selected_technologies);
-            $f3->set('selected_industries', $selected_industries);
-            $f3->reroute('/summary');
-        }
-
-        // Render the mailing-list page
-        $view = new Template();
-        echo $view->render('views/mail.html');
-    });
+            $GLOBALS['controller']->mail($f3);
+        });
 
                 // Summary
     $f3->route('GET|POST /summary',
         function($f3) {
-        foreach($_SESSION as $key => $value) {
-            $f3->set($key, $value);
-        }
-        $view = new Template();
-        echo $view->render('views/summary.html');
-    });
+            $GLOBALS['controller']->summary($f3);
+        });
 
-// Run F3
     $f3->run();
